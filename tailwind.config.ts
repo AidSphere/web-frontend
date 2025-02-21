fa5ca2db7af914d7c0d201dcb2fa17c0dea3db09
ushan senarathna <ushansenarathna000@gmail.com>
2025-02-21 13:45:37 +0530

    chore: configure hero ui theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,6 +33,70 @@ export default {
   	}
   },
   plugins: [require("tailwindcss-animate"), heroui(
-	
+	{
+		layout: {
+			disabledOpacity: "0.3", // opacity-[0.3]
+			radius: {
+				small: 'calc(var(--radius) - 4px)',
+				medium: 'calc(var(--radius) - 2px)',
+				large: 'var(--radius)',
+			},
+			borderWidth: {
+			  small: "1px", // border-small
+			  medium: "1px", // border-medium
+			  large: "2px", // border-large
+			},
+			fontSize: {
+				tiny: "0.75rem", // text-tiny
+				small: "0.875rem", // text-small
+				medium: "1rem", // text-medium
+				large: "1.728rem", // text-large
+			  },
+			  lineHeight: {
+				tiny: "1rem", // text-tiny
+				small: "1.25rem", // text-small
+				medium: "1.5rem", // text-medium
+				large: "1.75rem", // text-large
+			  },
+		  },
+		themes:{
+			"light":{
+				extend: "dark",
+				colors: {
+						background: "#fffefe",
+						foreground: "#020817",
+						primary: {
+							50: '#fef1f7',
+							100: '#fee5f2',
+							200: '#ffcbe7',
+							300: '#ffa1d1',
+							400: '#ff7aba',
+							500: '#fa3a91',
+							600: '#ea186c',
+							700: '#cc0a53',
+							800: '#a80c44',
+							900: '#8c0f3c',
+						  DEFAULT: "#fa3a91",
+						  foreground: "#ffffff",
+						},
+						secondary:{
+							50: '#eef8ff',
+							100: '#d9eeff',
+							200: '#bce3ff',
+							300: '#8ed2ff',
+							400: '#59b8ff',
+							500: '#3298ff',
+							600: '#247ef5',
+							700: '#1462e1',
+							800: '#174fb6',
+							900: '#142b57',
+							DEFAULT: "#3298ff",
+						  	foreground: "#ffffff",
+						},
+						focus: "#8ed2ff",
+					  },
+			}
+		}
+	}
   )],
 } satisfies Config;
