09a6f9c8f28b83f1cfbfd704e5ae9d89b9aa0297
kaweesha-mr <kaweesha.mr@gmail.com>
2025-03-23 05:59:48 +0530

    style(admin): adjust padding for header in Drug Importer & Patient Requests page

--- src/app/admin/request-approval/drug-importer-requests/page.tsx
+++ src/app/admin/request-approval/drug-importer-requests/page.tsx
@@ -3,7 +3,7 @@ import { NavigationTabs } from '../../create-accounts/_components';
 const DonorRegRequest = () => {
   return (
     <div>
-      <h1 className='text-center text-3xl font-bold'>
+      <h1 className='p-5 text-center text-3xl font-bold'>
         Drug Importer & Patient Requests
       </h1>
       <div>
