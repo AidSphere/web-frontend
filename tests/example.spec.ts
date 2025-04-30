acb621a251dd72bf3f45fb340be995215f77f27b
yashoda-fr <fyashodha@gmail.com>
2025-04-30 10:32:39 +0530

    feat(tests): add validation check for patient name error message in sponsor page form

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -47,4 +47,30 @@ test('Clicking Sponsor Now navigates to sponsor page', async ({ browser }) => {
 
 });
 
+test('Clicking Sponsor Now navigates to sponsor page and click pay button without filling a field', async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto('http://localhost:3000/donor/home');
+
+  const SponsorBtn = page.locator('[data-testid="sponsor-button-3"]');
+
+  // Just click — don't wait for navigation
+  await SponsorBtn.click();
+
+  //Wait for the sponsor form itself (reliable check)
+  await page.waitForSelector('#amount', { state: 'visible' });
+  await page.waitForSelector('#patientName', { state: 'visible' });
+  await page.waitForSelector('#message', { state: 'visible' });
+
+  // Now fill values
+  await page.locator('#amount').fill('1000');
+  await page.locator('#message').fill('Get well soon');
+  await page.locator('#public-radio').click(); // Check the radio button for public visibility
+  await page.locator('button:text("Pay")').click(); // Submit the form
+  console.log(await page.locator('#patientName-error').textContent()); // Print the error message
+  expect(page.locator('#patientName-error')).toContainText('Patient'); // Assert error message
+
+});
+
 
