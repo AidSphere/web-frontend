69bf565f24028fa19dbbd2cec44d24e304cd89f2
kaweesha-mr <kaweesha.mr@gmail.com>
2025-05-04 03:59:58 +0530

    feat: Update NavUser component to include logout functionality and enhance ApiService error handling with token management

--- src/components/sideBar/nav-user.tsx
+++ src/components/sideBar/nav-user.tsx
@@ -17,9 +17,12 @@ import {
   useSidebar,
 } from '@/components/ui/sidebar';
 import { UserDetailsType } from './types/userDetailsType';
+import { useAuth } from '@/components/auth/AuthProvider';
 
 export function NavUser({ user }: { user: UserDetailsType }) {
   const { isMobile } = useSidebar();
+  const { logout } = useAuth();
+  
   return (
     <SidebarMenu>
       <SidebarMenuItem>
@@ -66,8 +69,8 @@ export function NavUser({ user }: { user: UserDetailsType }) {
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <LogOut />
+            <DropdownMenuItem onClick={logout}>
+              <LogOut className="mr-2 h-4 w-4" />
               Log out
             </DropdownMenuItem>
           </DropdownMenuContent>
